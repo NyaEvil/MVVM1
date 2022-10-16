0d8896b1f88f28696e20a24ab8b146075d14e220
Nya Evil <Felfelix81@gmail.com>
2022-10-16 13:30:09 +0300

    Добавьте файлы проекта.

---
+++ MVVM1/Infrastructure/Commands/LyambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using MVVM1.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM1.Infrastructure.Commands
+{
+    internal class LyambdaCommand : Command
+    {
+        public readonly Action<object> _Execute;
+        public readonly Func<object, bool> _CanExecute;
+
+        public LyambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
---
+++ MVVM1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using MVVM1.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Input;
+using MVVM1.Infrastructure.Commands;
+using System.ComponentModel;
+
+namespace MVVM1.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private DataTable dataslab;
+
+        public DataTable DataSlab
+        {
+            get => dataslab;
+            set
+            {
+                dataslab = value;
+                OnPropertyChange();
+            }
+        }
+
+
+
+        public ICommand UpdateButton { get; }
+        private bool UpdateCanExecute(object o) => true;
+        private void UpdateOnExecuted(object o)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=EVILCITADELPE\\NYAEVILSQL;Initial Catalog=MVVM;Integrated Security=True");
+            connection.Open();
+            SqlCommand com = new SqlCommand("SELECT 'Дата-время начала (план)', '№ задания', '№ ПЗ', 'Толщина (план)', 'Ширина (план)', 'Вес (план)', 'Длина (ПЗ)', 'Длина (по разметке)' FROM PracticeInfokom", connection);
+            SqlDataReader reader = com.ExecuteReader();
+            connection.Close();
+            connection.Dispose();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+        }
+
+        public MainWindowViewModel()
+        {
+            UpdateButton = new LyambdaCommand(UpdateOnExecuted, UpdateCanExecute);
+        }
+
+    }
+}
